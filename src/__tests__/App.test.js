62d1e3476cdbdf8a7df47cea8863ebae99c0a2a2
Muhamad Gafar <muhamad_gafar@hotmail.com>
2021-04-23 16:54:23 +0100

    Added filtering by Topics

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 import mockAxios from 'axios';
-import {whenStable, sampleGetDepartments, sampleReadDataJson} from '../test-utils/Utils'
+import {whenStable, sampleGetDepartments, sampleReadDataJson, sampleGetTopics} from '../test-utils/Utils'
 
 const sampleListTicketComponentHtml = '<div data-testid="tickets"><div class="ticket"><div class="ticket-content-wrapper"><div class="ticket-left-column"><h2><b>Title: </b>VDI Issues</h2><p><b>Description: </b>The VDI has decided to stop working again fml</p></div><div class="ticket-right-column"><button class="btn btn-info" data-testid="updatebutton1">Update</button><button class="btn btn-danger" data-testid="deletebutton1">Delete</button><button class="btn btn-light" data-testid="statusbutton1">Start Work</button><p><b>ID:</b> 1</p><p><b>Department:</b> FX</p><p><b>Topic:</b> Remote Connectivity</p><p><b>Author:</b> Muhamad Gafar</p><p><b>Created:</b> Tue, 13 Apr 2021 10:02:02 GMT</p><p><b>Updated:</b> Tue, 13 Apr 2021 10:02:03 GMT</p></div></div></div><div class="ticket"><div class="ticket-content-wrapper"><div class="ticket-left-column"><h2><b>Title: </b>Symphony is being a douche</h2><p><b>Description: </b>My Symphony has stopped receiving messages</p></div><div class="ticket-right-column"><button class="btn btn-info" data-testid="updatebutton2">Update</button><button class="btn btn-danger" data-testid="deletebutton2">Delete</button><button class="btn btn-light" data-testid="statusbutton2">Mark As Done</button><p><b>ID:</b> 2</p><p><b>Department:</b> CREDIT</p><p><b>Topic:</b> Messaging Issues</p><p><b>Author:</b> Not Muhamad Gafar</p><p><b>Created:</b> Tue, 13 Apr 2021 10:02:02 GMT</p><p><b>Updated:</b> Tue, 13 Apr 2021 10:02:03 GMT</p></div></div></div><div class="ticket"><div class="ticket-content-wrapper"><div class="ticket-left-column"><h2><b>Title: </b>Titley McTitleFace</h2><p><b>Description: </b>Description McDescriptionFace</p><p><b>Solution: </b>Restart machine</p></div><div class="ticket-right-column"><button class="btn btn-info" data-testid="updatebutton3" disabled="">Update</button><button class="btn btn-danger" data-testid="deletebutton3" disabled="">Delete</button><button class="btn btn-light disabled" data-testid="statusbutton3" disabled="">Done</button><p><b>ID:</b> 3</p><p><b>Department:</b> FX</p><p><b>Topic:</b> Remote Connectivity</p><p><b>Author:</b> Author McAuthorFace</p><p><b>Created:</b> Tue, 13 Apr 2021 10:02:02 GMT</p><p><b>Updated:</b> Tue, 13 Apr 2021 10:02:03 GMT</p></div></div></div></div>';
 
@@ -10,11 +10,12 @@ const mockProps = { history: { push: jest.fn() } };
 describe('App Integration Test', () => {
   test('generates tickets from readAll.json', async () => {
     mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleGetDepartments));
+    mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleGetTopics));
     mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleReadDataJson));
     render(<App {...mockProps}/>);
     await whenStable();
     const sampleTickets = screen.getByTestId("tickets");
     expect(sampleTickets).toContainHTML(sampleListTicketComponentHtml);
-    expect(mockAxios.get).toHaveBeenCalledTimes(2);
+    expect(mockAxios.get).toHaveBeenCalledTimes(3);
   });
 });
--- src/__tests__/ListTicketComponent.test.js
+++ src/__tests__/ListTicketComponent.test.js
@@ -2,13 +2,14 @@ import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
 import mockAxios from 'axios';
 import ListTicketComponent from '../components/ListTicketComponent';
-import {whenStable, sampleGetDepartments, sampleReadDataJson, sampleMarkAsInProgressJson, sampleDeleteDataJson} from '../test-utils/Utils'
+import {whenStable, sampleGetDepartments, sampleReadDataJson, sampleMarkAsInProgressJson, sampleDeleteDataJson, sampleGetTopics} from '../test-utils/Utils'
 
 const mockProps = { history: { push: jest.fn() } };
 
 describe('TicketsList', () => {
   test('test Create ticket', async () => {
     mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleGetDepartments));
+    mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleGetTopics));
     mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleReadDataJson));
     render(<ListTicketComponent {...mockProps}/>);
     await whenStable();
@@ -18,6 +19,7 @@ describe('TicketsList', () => {
 
   test('test Edit ticket', async () => {
     mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleGetDepartments));
+    mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleGetTopics));
     mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleReadDataJson));
     render(<ListTicketComponent {...mockProps}/>);
     await whenStable();
@@ -27,6 +29,7 @@ describe('TicketsList', () => {
 
   test('test Delete ticket', async () => {
     mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleGetDepartments));
+    mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleGetTopics));
     mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleReadDataJson));
     render(<ListTicketComponent {...mockProps}/>);
     await whenStable();
@@ -38,23 +41,55 @@ describe('TicketsList', () => {
     await whenStable();
   });
 
-  test('test change department ticket', async () => {
+  test('test change department/topic filter', async () => {
+    // Initial setup
     mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleGetDepartments));
+    mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleGetTopics));
     mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleReadDataJson));
     render(<ListTicketComponent {...mockProps}/>);
     await whenStable();
+    expect(mockAxios.get).toHaveBeenCalledTimes(3);
 
+    // Set filter as None
     mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleReadDataJson));
-    userEvent.selectOptions(screen.getByTestId('select-department'), 'All');
+    userEvent.selectOptions(screen.getByTestId('select-filter'), 'None');
     await whenStable();
+    expect(mockAxios.get).toHaveBeenCalledTimes(4);
 
+    // Set filter mode to departments
     mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleReadDataJson));
-    userEvent.selectOptions(screen.getByTestId('select-department'), '1');
+    userEvent.selectOptions(screen.getByTestId('select-filter'), 'Departments');
     await whenStable();
+    expect(mockAxios.get).toHaveBeenCalledTimes(5);
+
+    // Select a department
+    mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleReadDataJson));
+    userEvent.selectOptions(screen.getByTestId('select-department-topic'), '1');
+    await whenStable();
+    expect(mockAxios.get).toHaveBeenCalledTimes(6);
+
+    // Set filter mode to topics
+    mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleReadDataJson));
+    userEvent.selectOptions(screen.getByTestId('select-filter'), 'Topics');
+    await whenStable();
+    expect(mockAxios.get).toHaveBeenCalledTimes(7);
+
+    // Select a topic
+    mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleReadDataJson));
+    userEvent.selectOptions(screen.getByTestId('select-department-topic'), '1');
+    await whenStable();
+    expect(mockAxios.get).toHaveBeenCalledTimes(8);
+
+    // Select any topic
+    mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleReadDataJson));
+    userEvent.selectOptions(screen.getByTestId('select-department-topic'), 'All');
+    await whenStable();
+    expect(mockAxios.get).toHaveBeenCalledTimes(9);
   });
 
   test('test Mark as In Progress', async () => {
     mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleGetDepartments));
+    mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleGetTopics));
     mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleReadDataJson));
     render(<ListTicketComponent {...mockProps}/>);
     await whenStable();
@@ -68,6 +103,7 @@ describe('TicketsList', () => {
 
   test('test Add solution ticket', async () => {
     mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleGetDepartments));
+    mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleGetTopics));
     mockAxios.get.mockImplementationOnce(() => Promise.resolve(sampleReadDataJson));
     render(<ListTicketComponent {...mockProps}/>);
     await whenStable();
